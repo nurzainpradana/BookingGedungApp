0d628f60f9145e2dda6af7c78d5ae13143168a86
nurzainpradana <pradananurzain@gmail.com>
2021-07-07 23:07:58 +0700

    create Splash Screen activity & import library

---
+++ app/src/main/java/com/afifemwe/bookinggedung/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.afifemwe.bookinggedung.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.afifemwe.bookinggedung.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
